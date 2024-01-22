e18ea2766c67afb4762c776938c76b65955f053e
sahuf2003 <127684377+sahuf2003@users.noreply.github.com>
2024-01-22 18:15:57 +0530

    Update data.py

--- data.py
+++ data.py
@@ -9,11 +9,3 @@ response.raise_for_status()
 data= response.json()
 question_data = data['results']
 
-# question_data = {
-# #     "question" : data['results'][0]['question'],
-# #     "incorrect" : data['results'][0]['incorrect_answers'][0],
-# #     "correct": data['results'][0]['correct_answer']
-# # }
-#
-# question_data = {"question" :i['results'][0]['question'] for  in data}
-# print(question_data)
