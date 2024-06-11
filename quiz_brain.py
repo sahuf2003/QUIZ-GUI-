aee8a82c0c49d7f2d92c3e00e6253ffb412581ab
Sahuf Shaikh <127684377+sahuf2003@users.noreply.github.com>
2024-06-11 23:58:20 +0530

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,5 +1,3 @@
-import html
-
 class QuizBrain:
 
     def __init__(self, q_list):
@@ -16,8 +14,6 @@ class QuizBrain:
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
         return f"{self.question_number}: {q_text}"
-        # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        # self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
@@ -26,6 +22,4 @@ class QuizBrain:
             return True
         else:
             return False
-        #
-        # print(f"Your current score is: {self.score}/{self.question_number}")
-        # print("\n")
+
